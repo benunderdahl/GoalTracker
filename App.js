b855e55ec1df251348776a0c8a4434a925e84c26
Code Classroom <benjamin.underdahl@proton.me>
2025-10-02 14:51:37 -0500

    created goal tracker skeleton

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{marginBottom: 10, borderWidth: 5, borderColor: "blue"}}>Hello, World!</Text>
-      <Button title="pressed" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+      <TextInput style={styles.textInput} placeholder="enter some text" />
+      <Button title="Add"/>
+      </View>
+      <Text>List of Goals</Text>
+      <View></View>
       
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 100
   },
-});
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    width: '80%',
+    marginRight: 8,
+    padding: 8
+  }
+})
